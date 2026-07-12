6b2f6e4960838e59e005fd2cdf07b8c06190dfd9
rostyslav <rostyslav.ihnatenko@gmail.com>
2026-07-12 19:54:26 +0300

    feat(applications): register applications feature state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
+import { provideState } from '@ngrx/store';
+import { APPLICATIONS_FEATURE_KEY } from './features/applications/data-access/applications.selectors';
+import { applicationsReducer } from './features/applications/data-access/applications.reducer';
 
 export const routes: Routes = [
   {
     path: '',
+    providers: [provideState(APPLICATIONS_FEATURE_KEY, applicationsReducer)],
     loadComponent: () =>
       import('./features/applications/feature/board-page/board-page').then((m) => m.BoardPage),
   },
